ade83e4c4b8d2b62c45e002a7e4c6cd2fd37f3da
bzmrdh <2129073850@qq.com>
2026-01-19 22:13:15 +0800

    添加fastbootd

--- device.mk
+++ device.mk
@@ -6,3 +6,20 @@
 #
 
 LOCAL_PATH := device/oppo/OP4EA3
+
+PRODUCT_TARGET_VNDK_VERSION := 31
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Dynamic Partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+    
